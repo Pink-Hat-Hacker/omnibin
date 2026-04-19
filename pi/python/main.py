a5806e4a41dad358faa1be1d4b8cb9e436f156ca
ZYV <zyv@duke.edu>
2026-04-19 04:07:49 +0100

    changes, classification and camera work in dev env

--- pi/python/main.py
+++ pi/python/main.py
@@ -4,12 +4,21 @@ import numpy as np
 import requests
 from tflite_runtime.interpreter import Interpreter
 from adafruit_motorkit import MotorKit
+# pi camera mod3 fix
+from picamera2 import Picamera2
+#live streaming
+from flask import Flask, Response
+import threading
+app = Flask(__name__)
+latest_label = "..."
+latest_conf = 0.0
+
 
 # Init motors
 kit = MotorKit()
 
 # Load model
-interpreter = Interpreter(model_path="model/model.tflite")
+interpreter = Interpreter(model_path="model/omnibin_model.tflite")
 interpreter.allocate_tensors()
 
 input_details = interpreter.get_input_details()
@@ -18,11 +27,62 @@ output_details = interpreter.get_output_details()
 labels = ["plastic", "paper", "metal", "garbage"]
 
 # Camera
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0)
+# pi camera mod3 fix
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration())
+picam2.start()
+
+def get_frame():
+    frame = picam2.capture_array()
+    #debugs
+    print("Frame shape:", frame.shape)
+    return frame
+
+# Live streaming
+def generate_frames():
+    while True:
+        frame = picam2.capture_array()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+
+        #draw on img
+        cv2.putText(
+            frame,
+            f"{latest_label} {latest_conf:.2f}",
+            (10, 30),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2
+        )
+        #encode to jpeg
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 def classify(frame):
+    # Converting RGBA to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
+
+    # Resize
     img = cv2.resize(frame, (224, 224))
+    #img = cv2.resize(frame, (224, 224))
+
+    #normalizing
+    img = img.astype(np.float32) / 255.0
+
+    #OG
     input_data = np.expand_dims(img, axis=0).astype(np.float32)
+    # debug
+    print("Input shape:", input_data.shape)
 
     interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
@@ -65,18 +125,32 @@ def send_event(label, confidence):
         pass
 
 
+
+#streaming
+def run_server():
+    app.run(host='0.0.0.0', port=5000)
+
+threading.Thread(target=run_server, daemon=True).start()
+
+
 print("OmniBin running...")
 
 while True:
-    ret, frame = cap.read()
+    # pi camera mod3 fix
+    frame = get_frame()
 
-    if not ret:
-        continue
+    #ret, frame = cap.read()
 
-    # Simple trigger: every 3 seconds
-    time.sleep(3)
+    #if not ret:
+    #    continue
+
+    # Simple trigger: every # seconds
+    time.sleep(5)
 
     label, conf = classify(frame)
+
+    latest_label = label
+    latest_conf = conf
 
     print(f"[AI] {label} ({conf:.2f})")
 
